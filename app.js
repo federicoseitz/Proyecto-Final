601b954494dfb0953ed519efea1c4a39386dd49b
federicoseitz <federico.seitz@gmail.com>
2022-08-20 15:14:41 -0300

    21-8

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 require('dotenv').config();
 var session = require('express-session');
+var fileUpload = require('express-fileupload');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -45,6 +46,10 @@ secured = async (req,res,next) => {
     }
   }
 
+  app.use(fileUpload ({
+    useTempFiles: true,
+    tempFileDir: '/tmp/'
+  }));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
--- routes/admin/novedades.js
+++ routes/admin/novedades.js
@@ -2,9 +2,35 @@ var express = require('express');
 var router = express.Router();
 var novedadesModel = require('../../modules/novedadesModel');
 
+var util = require('util');
+var cloudinary = require('cloudinary').v2;
+const uploader = util.promisify(cloudinary.uploader.upload);
+
+//listar novedades
+
 router.get('/', async function (req, res, next) {
 
   var novedades = await novedadesModel.getNovedades();
+
+  novedades = novedades.map(novedad => {
+    if (novedad.img_id) {
+      const imagen = cloudinary.image(novedad.img_id, {
+        widht: 50,
+        hight: 50,
+        crop: 'fill'
+      });
+      return {
+        ...novedad,
+        imagen
+      }
+    }
+    else {
+      return {
+        ...novedad,
+        imagen: ''
+      }
+    }
+  });
 
   res.render('admin/novedades', {
     layout: 'admin/layout',
@@ -27,9 +53,21 @@ router.get('/agregar', (req, res, next) => {
 
 router.post('/agregar', async (req, res, next) => {
   try {
+
+    var img_id = '';
+
+    if (req.files && Object.keys(req.files).length > 0) {
+      imagen = req.files.imagen;
+      img_id = (await uploader(imagen.tempFilePath)).public_id;
+    }
+
+
     //console.log(req.body)
     if (req.body.titulo != "" && req.body.subtitulo != "" && req.body.cuerpo != "") {
-      await novedadesModel.insertNovedad(req.body);
+      await novedadesModel.insertNovedad({
+        ...req.body,
+        img_id
+      });
       res.redirect('/admin/novedades')
     } else {
       res.render('admin/agregar', {
@@ -61,23 +99,23 @@ router.get('/modificar/:id', async (req, res, next) => {
 router.post('/modificar', async (req, res, next) => {
   try {
     //console.log(req.body.id)
-      var obj = {
-          titulo: req.body.titulo,
-          subtitulo: req.body.subtitulo,
-          cuerpo: req.body.cuerpo
-      }
-
-      await novedadesModel.modificarNovedadById(obj, req.body.id);
-      res.redirect('/admin/novedades');
-    } 
-    catch (error) {
-      console.log(error)
-      res.render('admin/modificar', {
-        layout: 'admin/layout',
-        error: true,
-        message: 'No se modifico la novedad'
-      })
+    var obj = {
+      titulo: req.body.titulo,
+      subtitulo: req.body.subtitulo,
+      cuerpo: req.body.cuerpo
     }
+
+    await novedadesModel.modificarNovedadById(obj, req.body.id);
+    res.redirect('/admin/novedades');
+  }
+  catch (error) {
+    console.log(error)
+    res.render('admin/modificar', {
+      layout: 'admin/layout',
+      error: true,
+      message: 'No se modifico la novedad'
+    })
+  }
 });
 
 
